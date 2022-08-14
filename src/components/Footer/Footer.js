1959ee5527edc731aa08ca338b92b698238de8fc
unknown <sifatniloy18@gmail.com>
2022-08-13 14:04:25 -1200

    footer fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,29 +8,29 @@ const Footer = () => {
 
                 <h2>Start learning from today</h2>
                 <br />
-                <div className='footer-points'>
-                    <p>Business</p>
-                    <p>Be an instructor</p>
-                    <p>Be an instructor</p>
-                    <p>Get app</p>
-                    <p>About us</p>
+                <div className='points'>
+                    <div className='footer-points'>
+                        <p>Business</p>
+                        <p>Be an instructor</p>
+                        <p>Get app</p>
+                        <p>About us</p>
 
-                </div>
-                <div className="footer-points">
-                    <p>Career</p>
-                    <p>Blog</p>
-                    <p>Help and Support</p>
-                    <p>Contact us</p>
-                    <p>Affiliate</p>
-                </div>
-                <div className="footer-points">
-                    <p>Terms</p>
-                    <p>Privacy policy</p>
-                    <p>Cookie Settings</p>
-                    <p>Sitemap</p>
+                    </div>
+                    <div className="footer-points">
+                        <p>Career</p>
+                        <p>Help and Support</p>
+                        <p>Contact us</p>
+                        <p>Affiliate</p>
+                    </div>
+                    <div className="footer-points">
+                        <p>Terms</p>
+                        <p>Privacy policy</p>
+                        <p>Cookie Settings</p>
+                        <p>Sitemap</p>
+                    </div>
                 </div>
                 <br />
-                <br />
+
                 <small>© by Netlearner | 2022 </small>
             </footer>
         </div>
